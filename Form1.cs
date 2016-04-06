e4b65f410983b8847892b606c9d5ee26c611d0a7
nbowman20 <nbowman20@users.noreply.github.com>
2016-04-05 20:12:11 -0400

    Added files via upload

---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Person
+        {
+            private string mFirstName;
+            private string mLastName;
+            private string mEmail;
+            private string mStatus;
+            private int mGraderNumber;
+
+            public void setFirstName(string firstName)
+            {
+                mFirstName = firstName;
+            }
+            public string getFirstName()
+            {
+                return mFirstName;
+            }
+
+            public void setLastName(string lastName)
+            {
+                mLastName = lastName;
+            }
+            public string getLastName() 
+            {
+                return mLastName;
+            }
+
+            public void setEmail(string email)
+            {
+                mEmail = email;
+            }
+            public string getEmail()
+            {
+                return mEmail;
+            }
+
+            public void setStatus(string status)
+            {
+                mStatus = status;
+            }
+            public string getStatus()
+            {
+                return mStatus;
+            }
+
+            public void setGraderNumber(int graderNumber)
+            {
+                mGraderNumber = graderNumber;
+            }
+            public int getGraderNumber()
+            {
+                return mGraderNumber;
+            }
+        }
+
+        public class User : Person
+        {
+            private string mUserID;
+            private string mUserName;
+            private string mPassword;
+            //private Team mTeamID = new Team();
+            //private Role mRoles = new Role();
+
+            public void setUserID(string userID)
+            {
+                mUserID = userID;
+            }
+            public string getUserID()
+            {
+                return mUserID;
+            }
+
+            public void setUserName(string userName)
+            {
+                mUserName = userName;
+            }
+            public string getUserName()
+            {
+                return mUserName;
+            }
+
+            public void setPassword(string password)
+            {
+                mPassword = password;
+            }
+            public string getPassword()
+            {
+                return mPassword;
+            }
+        }
+
+        public class Admin : User
+        {
+
+        }
+
+        public class Team
+        {
+           
+        }
+
+        public class Role
+        {
+            private string mRoleID;
+            private string mName;
+            private string mDescription;
+
+
+            public void setRoleID(string roleID)
+            {
+                mRoleID = roleID;
+            }
+            public string getRoleID()
+            {
+                return mRoleID;
+            }
+
+            public void setName(string name)
+            {
+                mName = name;
+            }
+            public string getName()
+            {
+                return mName;
+            }
+
+            public void setDescription(string description)
+            {
+                mDescription = description;
+            }
+            public string getDescription()
+            {
+                return mDescription;
+            }
+
+        }
+
+        public class Template
+        {
+            private string mTemplateID;
+            //Role
+            //Questions
+
+            public void setTemplateID(string templateID)
+            {
+                mTemplateID = templateID;
+            }
+            public string getTemplateID()
+            {
+                return mTemplateID;
+            }
+        }
+
+        public class Assessment
+        {
+            private string mAssessmentID;
+            //mTemplateID
+            private string mReviewee;
+            private string mReviewer;
+
+            public void setAssessmentID(string assessmentID)
+            {
+                mAssessmentID = assessmentID;
+            }
+            public string getAssessmentID()
+            {
+                return mAssessmentID;
+            }
+
+            public void setReviewee(string reviewee)
+            {
+                mReviewee = reviewee;
+            }
+            public string getReviewee()
+            {
+                return mReviewee;
+            }
+
+            public void setReviewer(string reviewer)
+            {
+                mReviewer = reviewer;
+            }
+            public string getReviewer()
+            {
+                return mReviewer;
+            }
+        }
+
+        public class MultipleChoice
+        {
+
+        }
+
+        public class FreeResponse
+        {
+
+        }
+
+        public interface Question
+        {
+            public void setQuestion(string question)
+            {
+
+            }
+            public string getQuestion()
+            {
+                
+            }
+
+            public void setAnswers()
+            {
+
+            }
+            public string getAnswers()
+            {
+
+            }
+
+            public string getType()
+            {
+
+            }
+        }
+    }
+}
